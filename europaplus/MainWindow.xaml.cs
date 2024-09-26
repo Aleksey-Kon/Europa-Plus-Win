51728e366ee3222ed696fb11fe4d563c2569b7b8
Aleksey-Kon <windowsviser@gmail.com>
2024-09-26 11:40:39 +0300

    main

---
+++ europaplus/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using WMPLib;
+
+namespace europaplus
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WindowsMediaPlayer wmPlayer = new WindowsMediaPlayer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.URL = @"https://ep256.hostingradio.ru:8052/europaplus256.mp3"; ;
+            wmPlayer.controls.play();
+            button2.Visibility = Visibility.Visible;
+            button1.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void button1_Click_2(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            button1.Visibility = Visibility.Visible;
+            button2.Visibility = Visibility.Collapsed;
+        }
+    }
+}
